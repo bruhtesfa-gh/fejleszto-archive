dd5f90185b013180dc9348eef9fc4be5f985b1eb
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2023-02-09 12:48:20 +0300

    navigation

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
 
     return <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="#/">Navbar</a>
+            <a className="navbar-brand" href="#/">Fejleszto Archive</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={toggleNav}>
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -35,11 +35,11 @@ const Header = () => {
                         <a className="nav-link" href="#/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#/">Link</a>
+                        <a className="nav-link" href="#/">Export</a>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search Your UGC" aria-label="Search Your UGC" />
                     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form>
                 <ul className="navbar-nav">
