e39154a0251c114854cf5f778f7e119702edc6d3
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2023-02-09 13:12:37 +0300

    css all done with zoom

--- client/src/components/story/story.jsx
+++ client/src/components/story/story.jsx
@@ -4,16 +4,24 @@ const Story = (props) => {
     const [story, SetStory] = useState(props.story);
 
     return <React.Fragment>
-        <div className="mx-2 story-container" style={{ backgroundImage: `url(${story.picture})` }}>
+        <div className="mx-2 story-container" >
+            <div className="story-image-container" >
+                <div className="story-image" style={{ backgroundImage: `url(${story.picture})` }}>
+                </div>
+            </div>
             <div className="story-holder ">
-                <div className="tagger_profile_container">
-                    <div className="tagger_profile_image" style={{ backgroundImage: `url(${story.profile})` }}>
+                <div className="tagger_profile_container zoom">
+                    <div className="tagger_profile_image " style={{ backgroundImage: `url(${story.profile})` }}>
 
                     </div>
                 </div>
                 <div className="tagger_name">
                     <span>{story.name}</span>
                 </div>
+            </div>
+
+            <div className="story-cover">
+
             </div>
         </div>
     </React.Fragment>
