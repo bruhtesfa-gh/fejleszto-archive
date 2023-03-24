01ea039eabaff880156fdcc2bc9c6bb93d8a2c51
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2023-03-24 13:44:10 +0300

    back to get

--- server/index.js
+++ server/index.js
@@ -29,10 +29,16 @@ app.get('/insta-auto-replay', async function (req, res) {
         'message': 'recived'
     })
 })
+app.get('/webhooks', async function (req, res) {
+
+    console.log(req);
+    res.send(req.query['hub.challenge']);
+});
 
 app.post('/webhooks', async function (req, res) {
-    console.log(req.body);
-    res.send("fsdag");
+
+    console.log(req);
+    //res.send(req.query['hub.challenge']);
 })
 
 
